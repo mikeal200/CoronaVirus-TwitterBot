2b80c701a6fcb1049350fa1f22d11234127c1d6c
Michael Williams <58800020+mikeal200@users.noreply.github.com>
2020-05-17 18:22:54 -0400

    rev 2.0 in progress new confirmed cases were off

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import requests
 import json
 import time
 from oauth import api
-from read_write import read_last_seen, store_last_seen
+from read_write import read_last_seen, store_last_seen, store_prev_confirmed, read_prev_confirmed
 
 while True:
     class Post:
@@ -29,26 +29,39 @@ while True:
 
 
     #request to corona-api
-    FILE_NAME = "last_seen.txt"
+    LAST_SEEN = "last_seen.txt"
+    PREV_CONFIRMED = "previous_confirmed.txt"
 
     request = requests.get('http://corona-api.com/countries/US', timeout=2.50)
     parsed = json.loads(request.content.decode('UTF-8'))
 
     date = parsed['data']['timeline'][0]['date']
 
-    if read_last_seen(FILE_NAME) != date:
-        store_last_seen(FILE_NAME, date)
-        print(read_last_seen(FILE_NAME) + " has been added to last_seen.txt")
+    if read_last_seen(LAST_SEEN) != date:
+        store_last_seen(LAST_SEEN, date)
+        print(read_last_seen(LAST_SEEN) + " has been added to last_seen.txt")
         population = parsed['data']['population']
         active = parsed['data']['timeline'][0]['active']
         confirmed = parsed['data']['timeline'][0]['confirmed']
         deaths = parsed['data']['timeline'][0]['deaths']
-        new_confirmed = parsed['data']['timeline'][0]['new_confirmed']
+        #new confirmed was giving innacurate statistics so i will save the previous days confirmed cases and it will subtracted by the current days confirmed cases
+        #new_confirmed = parsed['data']['timeline'][0]['new_confirmed']
         new_deaths = parsed['data']['timeline'][0]['new_deaths']
         new_recovered = parsed['data']['timeline'][0]['new_recovered']
         recovered = parsed['data']['timeline'][0]['recovered']
+        previous_confirmed = read_prev_confirmed(PREV_CONFIRMED)
+        new_confirmed = 0
+
+        if previous_confirmed == confirmed:
+            new_confirmed = 0
+        elif int(previous_confirmed) > confirmed:
+            previous_confirmed -= new_confirmed
+        else:
+            new_confirmed -= int(previous_confirmed)
+
         post = Post(population, active, confirmed, deaths, recovered, new_confirmed, new_deaths, new_recovered)
         #twitter api request
-        api.update_status(post.postFormat())
+        #api.update_status(post.postFormat())
+        print(post.postFormat())
 
-    time.sleep(1200)
+    #time.sleep(86400)
--- read_write.py
+++ read_write.py
@@ -9,3 +9,15 @@ def store_last_seen(FILE_NAME, last_seen_date):
     file_write.write(str(last_seen_date))
     file_write.close()
     return
+
+def store_prev_confirmed(FILE_NAME, prev_confirmed):
+    file_write = open(FILE_NAME, 'w')
+    file_write.write(str(prev_confirmed))
+    file_write.close()
+    return
+
+def read_prev_confirmed(FILE_NAME):
+    file_read = open(FILE_NAME, 'r')
+    prev_confirmed = str(file_read.read().strip())
+    file_read.close()
+    return prev_confirmed
