5363ba69965a61ce3e642b253c7357f5dfaabd83
micahel williams <willi6mc@mail.uc.edu>
2020-05-19 13:37:17 -0400

    tranfer to pc

--- main.py
+++ main.py
@@ -6,9 +6,7 @@ from read_write import read_last_seen, store_last_seen, store_prev_confirmed, re
 
 while True:
     class Post:
-        def __init__(self, population, active, confirmed, deaths, recovered, new_confirmed, new_deaths, new_recovered):
-            self.population = population    
-            self.active = active
+        def __init__(self, confirmed, deaths, recovered, new_confirmed, new_deaths, new_recovered):
             self.confirmed = confirmed
             self.deaths = deaths
             self.recovered = recovered
@@ -18,8 +16,6 @@ while True:
 
         def postFormat(self):
             return format("US Covid-19 Daily Statistics\n\n"\
-                "Population: " + "{:,}".format(population) + "\n"\
-                "Active Cases: " + "{:,}".format(active) + "\n"\
                 "Confirmed Cases: " + "{:,}".format(confirmed) + "\n"\
                 "Total Deaths: " + "{:,}".format(active) + "\n"\
                 "Total Recoveries: " + "{:,}".format(recovered) + "\n"\
@@ -35,14 +31,13 @@ while True:
     request = requests.get('https://api.covid19api.com/summary', timeout=2.50)
     parsed = json.loads(request.content.decode('UTF-8'))
 
-    date = parsed['countries']['timeline'][0]['date']
+    date = parsed['Countries'][177]['Date']
+    date = str(date).split('T', 1)[0]
 
     if read_last_seen(LAST_SEEN) != date:
         store_last_seen(LAST_SEEN, date)
         print(read_last_seen(LAST_SEEN) + " has been added to last_seen.txt")
-        population = parsed['data']['population']
-        active = parsed['data']['timeline'][0]['active']
-        confirmed = parsed['data']['timeline'][0]['confirmed']
+        confirmed = parsed['Countries']['timeline'][0]['confirmed']
         deaths = parsed['data']['timeline'][0]['deaths']
         #new confirmed was giving innacurate statistics so i will save the previous days confirmed cases and it will subtracted by the current days confirmed cases
         #new_confirmed = parsed['data']['timeline'][0]['new_confirmed']
@@ -52,14 +47,7 @@ while True:
         previous_confirmed = read_prev_confirmed(PREV_CONFIRMED)
         new_confirmed = 0
 
-        if previous_confirmed == confirmed:
-            new_confirmed = 0
-        elif int(previous_confirmed) > confirmed:
-            previous_confirmed -= new_confirmed
-        else:
-            new_confirmed -= int(previous_confirmed)
-
-        post = Post(population, active, confirmed, deaths, recovered, new_confirmed, new_deaths, new_recovered)
+        post = Post(confirmed, deaths, recovered, new_confirmed, new_deaths, new_recovered)
         #twitter api request
         #api.update_status(post.postFormat())
         print(post.postFormat())
